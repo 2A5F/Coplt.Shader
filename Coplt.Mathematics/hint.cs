1214b9546186e34a1f745ffbfa09341cacc206e6
U2A5F <U2A5F@outlook.com>
2024-05-01 19:58:47 +0800

    vector merge

--- Coplt.Mathematics/hint.cs
+++ Coplt.Mathematics/hint.cs
@@ -1,3 +1,17 @@
 ﻿namespace Coplt.Mathematics;
 
 internal struct is_scalar;
+
+internal struct insert_y;
+
+internal struct insert_z;
+
+internal struct insert_yz;
+
+internal struct insert_yw;
+
+internal struct insert_xz;
+
+internal struct merge_xw_yz;
+
+internal struct merge_xz_yw;
