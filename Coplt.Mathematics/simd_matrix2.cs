4e3876315f230a79f585e3ae7c47b564f5a9ae58
U2A5F <U2A5F@outlook.com>
2024-05-18 17:58:34 +0800

    simd transpose 4x4

---
+++ Coplt.Mathematics/simd_matrix2.cs
@@ -0,0 +1,12 @@
+﻿#if NET8_0_OR_GREATER
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Coplt.Mathematics;
+
+public static partial class simd_matrix
+{
+    
+}
+
+#endif
--- MathTests/Test1.cs
+++ MathTests/Test1.cs
@@ -9,6 +9,19 @@ public class Test1
 {
     #if NET8_0_OR_GREATER
     [Test]
+    public void Transpose4x4()
+    {
+        var c0 = new long4(1, 2, 3, 4).UnsafeGetInner();
+        var c1 = new long4(5, 6, 7, 8).UnsafeGetInner();
+        var c2 = new long4(9, 10, 11, 12).UnsafeGetInner();
+        var c3 = new long4(13, 14, 15, 16).UnsafeGetInner();
+        var r = simd_matrix.Transpose4x4(c0, c1, c2, c3);
+        Console.WriteLine(r.Item1);
+        Console.WriteLine(r.Item2);
+        Console.WriteLine(r.Item3);
+        Console.WriteLine(r.Item4);
+    }
+    [Test]
     public void Float()
     {
         var a = new float4(9);
