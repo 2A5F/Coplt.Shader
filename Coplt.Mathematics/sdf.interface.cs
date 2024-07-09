b4c7347ef17626f166a68a5a57274248ed7ccbdb
U2A5F <U2A5F@outlook.com>
2024-07-09 16:22:06 +0800

    some sdf

---
+++ Coplt.Mathematics/sdf.interface.cs
@@ -0,0 +1,9 @@
+﻿namespace Coplt.Mathematics.Sdf;
+
+public interface ISdfShape<out T, in V> where V : IVector<T>
+{
+    public T calc(V target);
+}
+
+public interface ISdfShape2d<out T, in V> : ISdfShape<T, V> where V : IVector2<T> { }
+public interface ISdfShape3d<out T, in V> : ISdfShape<T, V> where V : IVector3<T> { }
